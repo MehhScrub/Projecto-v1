2ef6f648135b5c9d696034c03446e16a48de999e
MehhScrub <38139122+MehhScrub@users.noreply.github.com>
2020-08-06 12:22:53 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import Main from "./Main.js";
+import AboutUs from "./AboutUs";
+import Faq from "./Faq.js";
+import Contactus from "./Contactus";
+import Profile from "./Profile";
+import ProfileBS from "./ProfileBS";
+import "bootstrap/dist/css/bootstrap.css";
+import ProfileFlex from "./ProfileFlex";
+import Testing from "./Testing";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<ProfileFlex />, document.getElementById("root"));
